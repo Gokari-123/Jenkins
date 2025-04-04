d906f24188a9ae4c1e000e40e0b708678b7c5ab5
Gokari-123 <mahaboobgokari@gmail.com>
2025-04-04 12:54:23 +0530

    initial commit

--- src/main/java/org/example/A.java
+++ src/main/java/org/example/A.java
@@ -1,4 +1,11 @@
 package org.example;
 
+import java.util.Date;
+
 public class A {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("This is a test for the new branch.");
+        System.out.println(new Date());
+    }
 }
